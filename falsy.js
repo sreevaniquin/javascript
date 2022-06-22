f85609132818447fd87d46b7a412cb38c9d86e9b
sreevaniquin <107997558+sreevaniquin@users.noreply.github.com>
2022-06-22 17:01:02 +0530

    Add files via upload

---
+++ falsy.js
@@ -0,0 +1,13 @@
+var user=null;
+if(!user){
+    console.log("condition is true");
+}
+var user1="";
+if(user1){
+    console.log("condition is true");
+}
+console.log(2+"2");
+var user="2";
+if(2==user){
+    console.log("condition is true");
+}
---
+++ variables.js
@@ -0,0 +1,28 @@
+/*var name="java script";
+var courseName="java script camp";
+var isLoggedIn=false;
+var loggedCount=34;
+var payMode;
+console.log(payMode);
+console.log(loggedCount);
+console.log("loggedCount");
+payMode="credit card";*/
+
+const uid="abc123";
+var fullName="java script";
+var email="abc@xyz.com";
+var password="123456";
+var confirmpass="123456";
+var coursecount=0;
+var isLoggedInFromGoogle=false;
+
+//var name=prompt("Enter your name");
+console.log("full name is:"+name);
+/*console.log("uid is:",uid);
+console.log(`
+With Unique Id: ${uid}
+User Is:${fullName}
+and his email is:${email}
+and uses the password:${password}
+`);*/
+
